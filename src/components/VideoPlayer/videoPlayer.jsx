ff750fa33851873b5ea16ff666b224951fc8b440
Ambrose Blay <ambrose2002blay@gmail.com>
2024-02-04 11:58:25 -0500

    working on streaming

--- src/components/VideoPlayer/videoPlayer.jsx
+++ src/components/VideoPlayer/videoPlayer.jsx
@@ -4,8 +4,9 @@ const VideoPlayer = ( ) => {
 	var videoId = useParams()
 	videoId = videoId.videoId
 	return (
-		<video id="videoPlayer" controls muted autoPlay playsInline>
+		<video id="videoPlayer" controls muted>
 			<source src= {`https://desolate-bayou-93955-64c6896ee0b5.herokuapp.com/api/video/${videoId}`} type="video/mp4" />
+			Video not Supported
 		</video>
 	);
 };
