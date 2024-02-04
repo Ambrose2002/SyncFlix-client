99557991e8f114ab394b2a552dcdd59fb7651bc4
Ambrose Blay <ambrose2002blay@gmail.com>
2024-02-04 12:46:13 -0500

    working on streaming

--- src/components/VideoPlayer/videoPlayer.jsx
+++ src/components/VideoPlayer/videoPlayer.jsx
@@ -1,12 +1,77 @@
+// // VideoPlayer.js
+
+// import React, { useEffect, useRef, useState } from 'react';
+// import io from 'socket.io-client';
+// import ReactPlayer from 'react-player';
+// import { useParams } from 'react-router-dom';
+
+// const VideoPlayer = () => {
+// 	const { videoId } = useParams();
+// 	const playerRef = useRef(null);
+// 	const [isPlaying, setIsPlaying] = useState(false);
+
+// 	useEffect(() => {
+// 		const socket = io(`/api/video/${videoId}`);  // Replace with your Express server URL
+
+// 		const playVideo = () => {
+// 			setIsPlaying(true);
+// 			setTimeout(() => {
+// 				setIsPlaying(false);
+// 				socket.emit('video_stream', { currentTime: playerRef.current.getCurrentTime() });
+// 			}, 50);
+// 		};
+
+// 		socket.on('video_frame', (data) => {
+// 			playerRef.current.seekTo(data.currentTime);
+// 		});
+
+// 		document.getElementById('playButton').addEventListener('click', playVideo);
+
+// 		return () => {
+// 			socket.disconnect();
+// 			document.getElementById('playButton').removeEventListener('click', playVideo);
+// 		};
+// 	}, []);
+
+// 	return (
+// 		<div>
+// 			<button id="playButton">Play Video</button>
+// 			<ReactPlayer
+// 				ref={playerRef}
+// 				url={`/api/video/${videoId}`}
+// 				playing={isPlaying}
+// 				controls={true}
+// 				width="100%"
+// 				height="auto"
+// 				muted
+// 				style={{ display: 'none' }}
+// 			/>
+// 		</div>
+// 	);
+// };
+
+// export default VideoPlayer;
+
 import { useParams } from "react-router-dom";
+import ReactPlayer from 'react-player'
 
 const VideoPlayer = ( ) => {
 	var videoId = useParams()
 	videoId = videoId.videoId
 	return (
-		<video id="videoPlayer" controls>
-			<source src= {`/api/video/${videoId}`} type="video/mp4" />
-		</video>
+		// <video id="videoPlayer" controls>
+		// 	<source src= {`/api/video/${videoId}`} type="video/mp4" />
+		// </video>
+		<ReactPlayer
+				// ref={playerRef}
+				url={`/api/video/${videoId}`}
+				// playing={isPlaying}
+				controls={true}
+				// width="100%"
+				// height="auto"
+				// muted
+				// style={{ display: 'none' }}
+			/>
 	);
 };
 
