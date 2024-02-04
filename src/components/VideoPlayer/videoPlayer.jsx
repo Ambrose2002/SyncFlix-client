2b87287268101549b3cf4f50fb265273370da332
Ambrose Blay <ambrose2002blay@gmail.com>
2024-02-04 12:46:45 -0500

    working on streaming

--- src/components/VideoPlayer/videoPlayer.jsx
+++ src/components/VideoPlayer/videoPlayer.jsx
@@ -1,13 +1,38 @@
+// import { useParams } from "react-router-dom";
+
+// const VideoPlayer = ( ) => {
+// 	var videoId = useParams()
+// 	videoId = videoId.videoId
+// 	return (
+// 		<video id="videoPlayer" controls muted>
+// 			<source src= {`https://desolate-bayou-93955-64c6896ee0b5.herokuapp.com/api/video/${videoId}`} type="video/mp4" />
+// 			Video not Supported
+// 		</video>
+// 	);
+// };
+
+// export default VideoPlayer;
+
 import { useParams } from "react-router-dom";
+import ReactPlayer from 'react-player'
 
 const VideoPlayer = ( ) => {
 	var videoId = useParams()
 	videoId = videoId.videoId
 	return (
-		<video id="videoPlayer" controls muted>
-			<source src= {`https://desolate-bayou-93955-64c6896ee0b5.herokuapp.com/api/video/${videoId}`} type="video/mp4" />
-			Video not Supported
-		</video>
+		// <video id="videoPlayer" controls>
+		// 	<source src= {`/api/video/${videoId}`} type="video/mp4" />
+		// </video>
+		<ReactPlayer
+				// ref={playerRef}
+				url={`/api/video/${videoId}`}
+				// playing={isPlaying}
+				controls={true}
+				// width="100%"
+				// height="auto"
+				// muted
+				// style={{ display: 'none' }}
+			/>
 	);
 };
 
